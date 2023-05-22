01b5a2ff87c357c6837e549e690e3b8733e8a1f3
MarlenAmir <amirmarlen7@gmail.com>
2023-05-22 20:46:51 +0600

    adding a shimmer effect on loading

--- lib/widgets/foreground_widget.dart
+++ lib/widgets/foreground_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:diploma_work/screens/booking_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:diploma_work/model/firebase_data.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ForegroundWidget extends StatefulWidget {
   const ForegroundWidget({super.key, required this.searchController});
@@ -28,7 +29,66 @@ class _ForegroundWidgetState extends State<ForegroundWidget> {
             }
             switch (snapshot.connectionState) {
               case ConnectionState.waiting:
-                return const CircularProgressIndicator();
+                return SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Shimmer.fromColors(
+                    baseColor: Colors.grey[300]!,
+                    highlightColor: Colors.grey[100]!,
+                    child: Column(
+                      children: List.generate(
+                        5, // Adjust the number of shimmering items as needed
+                        (index) => Container(
+                          width: double.infinity,
+                          margin: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color.fromARGB(255, 238, 240, 241),
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 250,
+                                color: Colors.grey, // Placeholder color
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 200,
+                                height: 14,
+                                color: Colors.grey, // Placeholder color
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 100,
+                                height: 14,
+                                color: Colors.grey, // Placeholder color
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 150,
+                                height: 14,
+                                color: Colors.grey, // Placeholder color
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 120,
+                                height: 14,
+                                color: Colors.grey, // Placeholder color
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 340,
+                                height: 40,
+                                color: Colors.grey, // Placeholder color
+                              ),
+                              const SizedBox(height: 10),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
               default:
                 return SingleChildScrollView(
                   scrollDirection: Axis.vertical,
